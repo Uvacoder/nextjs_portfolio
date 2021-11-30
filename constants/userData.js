b8154681c6b17abe9378ac266f43134a1b01d807
Dax Wann <daxwann@gmail.com>
2021-11-29 16:52:34 -0800

    add user data

---
+++ constants/userData.js
@@ -0,0 +1,72 @@
+const userData = {
+  githubUsername: "daxwann",
+  name: "Dax Wann",
+  designation: "Full-Stack Software Engineer",
+  avatarUrl: "/avatar.png",
+  email: "daxwann at gmail dot com",
+  address: "San Jose, California",
+  projects: [
+    {
+      title: "Yelp Camp",
+      link: "https://github.com/daxwann/Yelp-camp",
+      imgUrl: "/yelp-camp.png",
+    }
+  ],
+  about: {
+    title:
+      "I'm a software engineer passionate about designing efficient and clean ways of programming to solve complex problems.",
+    description: [
+      `I have years of work experience in building single-page applications and RESTful web services.`,
+      `Fluent in ES6, Ruby, Java, and SQL. Skilled in working with React, Node.js Express, Rails, Spring Boot, PostgreSQL, MongoDB, and more.`,
+      `I’m committed to supporting a collaborative and diverse work environment.`,
+    ],
+    currentProject: "Tailwind Master Kit",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Software Engineer 2",
+      company: "PayPal",
+      year: "2020 - Present",
+      companyLink: "https://paypal.com",
+      desc: "Developing internal applications in React/Redux, Express, and Spring Boot",
+    },
+    {
+      title: "Full-Stack Software Engineer",
+      company: "Hwy Haul",
+      year: "2020",
+      companyLink: "https://hwyhaul.com",
+      desc: "Developed customer-facing application through agile process. Tech stack includes React & Redux, Java Spring Boot, MySQL, and Redis.",
+    },
+    {
+      title: "Web Developer",
+      company: "Lawgood",
+      year: "2018",
+      companyLink: "https://lawgood.io",
+      desc: "Developed custom UI components with HTML5, CSS3, and ES6. Enhanced SEO by increasing load speed and crawl accessibility.",
+    },
+    {
+      title: "Master's Degree in Information Science",
+      company: "San Jose State University",
+      year: "2013",
+      companyLink: "https://ischool.sjsu.edu/",
+      desc: "Specialized in web development, metadata, taxonomy, and digital archiving",
+    },
+    {
+      title: "Bachelor's Degree in Art History",
+      company: "UC San Diego",
+      year: "2010",
+      companyLink: "https://visarts.ucsd.edu/",
+      desc: "Graduated Summa Cum Laude",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1ECCG_-JEn6tKZ-8IVJUFbFpbosAorMAD/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/hyperarttransferprotocol",
+    linkedin: "https://linkedin.com/in/daxwann",
+    github: "https://github.com/daxwann",
+  },
+};
+
+export default userData;
